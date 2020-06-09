eebf74eacb2812a98e2be9f0f80b2fab8855ea57
willypelz <pelumiasefon@gmail.com>
2020-06-09 18:56:09 +0100

    add books controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/*******************************************
+ *******************************************
+ *****  Books API Version 1  ***************
+ *******************************************
+ *******************************************/
+
+
+Route::namespace('Api\v1')->prefix('v1')->group(function () {
+    Route::group(['prefix' => 'books', 'namespace' => 'Tenant\StudentParentApi'], function () {
+//        Route::resource('/', function () {
+//            return 'hello';
+//        });
+    });
 });
