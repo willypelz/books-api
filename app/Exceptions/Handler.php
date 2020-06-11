6962b46649ce22fd5501bb3df63271a47754f471
willypelz <pelumiasefon@gmail.com>
2020-06-11 01:00:04 +0100

    add more exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use App\Http\Library\RestFullResponse\ApiResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +55,25 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        //Handle Validation exceptions
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception,$request);
+        }
+
+        //Handle model exceptions
+        if($exception instanceof ModelNotFoundException){
+            $modelName = strtolower(class_basename($exception->getModel()));
+            return (new ApiResponse)->respondWithError("No $modelName exists with this identifier");
+        }
+
+        //Handle NotFoundHTTP exception
+        if($exception instanceof NotFoundHttpException){
+            return (new ApiResponse)->respondWithError("The specified URL cannot be found");
+        }
+
+        if(config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+        return (new ApiResponse)->respondWithError('Unexpected Exception. Try later');
     }
 }
